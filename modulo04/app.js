38d0699581ed1f303722f121f3eae77634a72f52
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-04 18:24:17 -0300

    Adds list and create accounts route

--- modulo04/app.js
+++ modulo04/app.js
@@ -3,19 +3,19 @@ import { accountsRouter } from "./routes/accounts.js";
 import mongoose from "mongoose";
 import fs from "fs";
 
-(async () => {
-  try {
-    fs.readFile("accounts.json", "utf8", (err, data) => {
-      if (err) {
-        console.warn(`Error: ${err}`);
-      }
-      const accounts = JSON.parse(data);
-      console.log(accounts);
-    });
-  } catch (error) {
-    console.log(`Error opening file: ${error}`);
-  }
-})();
+// (async () => {
+//   try {
+//     fs.readFile("accounts.json", "utf8", (err, data) => {
+//       if (err) {
+//         console.warn(`Error: ${err}`);
+//       }
+//       const accounts = JSON.parse(data);
+//       console.log(accounts);
+//     });
+//   } catch (error) {
+//     console.log(`Error opening file: ${error}`);
+//   }
+// })();
 
 (async () => {
   try {
--- modulo04/routes/accounts.js
+++ modulo04/routes/accounts.js
@@ -1,11 +1,32 @@
 import express from "express";
+import { accountModel } from "../models/account.js";
 
 const accountsRouter = express.Router();
 
 accountsRouter.use(express.json());
 
-accountsRouter.get("/accounts", (req, res) => {
-  res.send({ ok: true });
+// accountsRouter.get("/accounts", (req, res) => {
+//   res.send({ ok: true });
+// });
+
+accountsRouter.get("/accounts", async (_, res) => {
+  try {
+    const account = await accountModel.find({});
+    res.send(account);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+accountsRouter.post("/accounts", async (req, res) => {
+  try {
+    const account = new accountModel(req.body);
+    await account.save();
+
+    res.send(account);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 export { accountsRouter };
