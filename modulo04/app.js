ff99b84afd94e27b408d81c2c033751b1c5be5fe
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-04 14:58:38 -0300

    Adds connection to local MongoDB instance

--- modulo04/app.js
+++ modulo04/app.js
@@ -1,5 +1,19 @@
 import express from "express";
 import { accountsRouter } from "./routes/accounts.js";
+import mongoose from "mongoose";
+
+(async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/accounts", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to MongoDB on port 27017");
+  } catch (error) {
+    console.log(`Error connecting to MongoDB: ${error}`);
+  }
+})();
 
 const app = express();
 
